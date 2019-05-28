44778f3bef8853d6caa584dcc67c303ce3639d98
Qxyat <qxyat2019@gmail.com>
2019-05-28 21:40:16 +0800

    revert context 判空逻辑

--- shell/platform/darwin/ios/ios_external_texture_gl.mm
+++ shell/platform/darwin/ios/ios_external_texture_gl.mm
@@ -61,7 +61,7 @@ void IOSExternalTextureGL::Paint(SkCanvas& canvas,
                                  CVOpenGLESTextureGetName(texture_ref_), GL_RGBA8_OES};
   GrBackendTexture backendTexture(bounds.width(), bounds.height(), GrMipMapped::kNo, textureInfo);
   sk_sp<SkImage> image =
-      SkImage::MakeFromTexture(context == nullptr ? canvas.getGrContext() : context, backendTexture,
+      SkImage::MakeFromTexture(context, backendTexture,
                                kTopLeft_GrSurfaceOrigin, kRGBA_8888_SkColorType,
                                kPremul_SkAlphaType, nullptr);
   FML_DCHECK(image) << "Failed to create SkImage from Texture.";
